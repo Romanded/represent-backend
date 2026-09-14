bd707e4c4d156207b63146fb8e28febf9d1802b6
Romanded <romanabo099@gmail.com>
2026-09-14 23:56:10 +0700

    fix vercel prisma

--- src/core/prisma/prisma.service.ts
+++ src/core/prisma/prisma.service.ts
@@ -3,15 +3,22 @@ import { Injectable } from '@nestjs/common';
 import { PrismaClient } from './generated/client.js';
 import { ConfigService } from '@nestjs/config';
 import { PrismaPg } from '@prisma/adapter-pg';
+import { Pool } from 'pg';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
   constructor(
     private configService: ConfigService
   ) {
-    const adapter = new PrismaPg({
+    const pool = new Pool({
       connectionString: configService.getOrThrow('COCKROACH_URI')
+      max: 1,
+      idleTimeoutMillis: 10000,
+      connectionTimeoutMillis: 10000,
     });
+
+
+    const adapter = new PrismaPg(pool);
     super({ adapter });
   }
 }
